a140922ad918473f9915c24b72987979eef86f35
Mohammad Nanda <gakonok@gmail.com>
2019-10-16 13:08:25 +0700

    update session di index

--- index.php
+++ index.php
@@ -64,7 +64,16 @@ session_start();
 								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
 								 aria-expanded="false">Akun</a>
 								<ul class="dropdown-menu">
-									<li class="nav-item"><a class="nav-link" href="#"><?php echo $_SESSION['name']; ?></a></li>
+								<?php if(isset($_SESSION['name'])){ 
+									?>
+									<li class="nav-item"><a class="nav-link" href="user.php"><?php echo $_SESSION['name']; ?></a></li>
+									<?php
+									} else{
+									?>
+									<li class="nav-item"><a class="nav-link" href="login.php"><?php echo "Daftar/Login"; ?></a></li>
+									<?php
+										
+									}?>
 									<li class="nav-item"><a class="nav-link" href="#">Lacak pesanan</a></li>
 									<li class="nav-item"><a class="nav-link" href="#">Riwayat</a></li>
 								</ul>
